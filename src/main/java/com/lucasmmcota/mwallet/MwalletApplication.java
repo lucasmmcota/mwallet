45e432e89f7b85e331dc56947aa2ab413300dcd4
Lucas Monteiro <lucasmmcota@gmail.com>
2024-05-17 21:00:58 -0300

    Iniciar projeto Spring Boot

---
+++ src/main/java/com/lucasmmcota/mwallet/MwalletApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmmcota.mwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasmmcota/mwallet/MwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmmcota.mwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
